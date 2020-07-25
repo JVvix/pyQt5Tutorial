f81e80d84c792d1810b5cfe34d5918bd00817c9d
JVvix <fayedotly@gmail.com>
2020-07-25 10:00:28 -0700

    finished second tutorial of pyQt5

--- pyQt5Tutorial.py
+++ pyQt5Tutorial.py
@@ -1,18 +1,35 @@
+# https://www.youtube.com/watch?v=Vde5SH8e1OQ&list=PLzMcBGfZo4-lB8MZfHPLTEHO9zJDDLpYj
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 200, 200)
+        self.setWindowTitle("PyQt5 Tutorial")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("my first label!")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click Me!")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("yay, you clicked me!")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("PyQt5 Tutorial")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("My First Label")
-    label.move(50, 50)
-
+    win = MyWindow()
     win.show()
     sys.exit(app.exec_())
 
